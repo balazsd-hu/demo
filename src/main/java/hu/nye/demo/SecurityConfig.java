535f7b014b7ea48677d3450d4a31743ac938d5cc
Dömötör Balázs <balazsd76@gmail.com>
2026-05-18 22:56:52 +0200

    security fine tuning required for sonarqube

--- src/main/java/hu/nye/demo/SecurityConfig.java
+++ src/main/java/hu/nye/demo/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+            //    .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**", "/admin.html").hasRole("ADMIN")
