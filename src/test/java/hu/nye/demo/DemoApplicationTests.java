c10fa9d0672ea6fea11c1facf8f08ec5e451efc6
Dömötör Balázs <balazsd76@gmail.com>
2026-05-20 11:54:22 +0200

    H2 és teszt

--- src/test/java/hu/nye/demo/DemoApplicationTests.java
+++ src/test/java/hu/nye/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package hu.nye.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Test
